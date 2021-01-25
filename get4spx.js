b818ee0975c62e1e3d6786fae2e969b22e59d07f
raylexlee <raylexym57lee@gmail.com>
2021-01-26 01:46:04 +0800

    Split ssml into smaller parts

---
+++ get4spx.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const voiceName = [ "zh-HK-HiuGaaiNeural", "zh-HK-HiuMaanNeural", "zh-HK-WanLungNeural" ];
+const voiceText = (voice, text) => `<voice name="${voice}">
+        ${text}
+    </voice>`;
+const ssml = (title, body) => `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="zh-HK">
+    ${title}
+    ${body}
+</speak>
+`;
+const subst = `<break time="100ms" />`;
+const regex = /<br>/mg;
+const pattern = /^.*color=navy>(.*)<\/br>(.*)<\/font><\/h4>\r\n\t\t\t(.*)$/m;
+const infile = i => `./redchamberdream/${i.toString()}.html`;
+const outfile = (i, j) => `./spx/${(i-398).toString().padStart(3,'0')}_${j}.ssml`;
+for (let i=399; i <= 518; i++) {
+  const rawdata = fs.readFileSync(infile(i), {encoding:'utf8', flag:'r'});
+  const m = rawdata.match(pattern);
+  const paras = m[3].replace(/<img src=\/mpf\/h\/HUHN\.BMP align=absmiddle border=0>/gm, '雞')
+                        .replace(/<img src=\/mpf\/h\/HUAU\.BMP align=absmiddle border=0>/gm,'巴')
+                        .split(regex)
+  paras.pop();
+  paras.pop();                      
+  const titleVoice = voiceText(voiceName[2], `${m[1]}${subst}${m[2]}`);
+  let j = 0;
+  let accPara = '';
+  let cnt = 0;
+  while (paras.length) {
+    if ((cnt + 2*(paras[0].length) + subst.length) > 4500) {
+      j++;
+      const bodyVoice =  voiceText(voiceName[1], accPara);
+      const title = (j === 1) ? titleVoice : '';
+      fs.writeFileSync(outfile(i, j), ssml(title, bodyVoice));
+      cnt = 0;
+      accPara = '';
+    }
+    const topPara = paras.shift();
+    accPara = `${accPara}${subst}${topPara}`;
+    cnt += 2*(topPara.length) + subst.length;
+  }
+  if (accPara) {
+      j++;
+      const bodyVoice =  voiceText(voiceName[1], accPara);
+      const title = (j === 1) ? titleVoice : '';
+      fs.writeFileSync(outfile(i, j), ssml(title, bodyVoice));
+  }
+}  
